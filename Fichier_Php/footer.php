3b81b47afbb4c43428dbff808e29188ac0aa493f
Fédi <fedielmanoubi@gmail.com>
2023-03-08 23:27:52 +0100

    commit Tp3php

--- Fichier_Php/footer.php
+++ Fichier_Php/footer.php
@@ -1,7 +1,7 @@
  </div>
         <div id="modalSupp"class="modal fade" tabindex="-1">
       <div class="modal-dialog">
-        <div class="modal-content">
+         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">Confirmation de Suppression</h5>
           </div>
